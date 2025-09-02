350425e5c1e1b5397304771bfb9310e6bd08c961
A-web-web <thornyno@gmail.com>
2025-09-02 09:14:31 -0700

    Updated

--- p2.py
+++ p2.py
@@ -1,11 +1,17 @@
 i = 1
 c = 3
+a=0
 b=[]
 while i <= c:
  try:
   a=int(input("Enter:")) 
   b.append(a)
   i+=1 
+  a=1
  except:
-  print("Error")
-print(b)
+  print("Error") 
+  break
+if(a==1):
+ print(b)
+
+
